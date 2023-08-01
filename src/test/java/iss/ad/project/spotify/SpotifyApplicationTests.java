fe1287440d77567c20cebf70742bf312abf7b90e
Su Latt <slsl24@Sus-iMac.local>
2023-08-01 22:01:30 +0800

    first commit

---
+++ src/test/java/iss/ad/project/spotify/SpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package iss.ad.project.spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
