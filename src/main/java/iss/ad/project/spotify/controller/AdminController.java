e5aaa436b0df68c3268e1aa2f903ebfd1797e1a8
Chit Su Wai Lin <e1114086@u.nus.edu>
2023-08-16 19:29:15 +0800

    validation part for controller

--- src/main/java/iss/ad/project/spotify/controller/AdminController.java
+++ src/main/java/iss/ad/project/spotify/controller/AdminController.java
@@ -1,17 +1,21 @@
 package iss.ad.project.spotify.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import iss.ad.project.spotify.model.Admin;
 import iss.ad.project.spotify.model.Task;
@@ -86,10 +90,31 @@ public class AdminController {
 		return "task-create";
 	}
     @PostMapping("/task/create")
-    public String createTaskSubmit(@ModelAttribute("task")Task task) {
-        adminSrv.create(task);
-        return "redirect:/taskList";
-        
+    public String createTaskSubmit(@ModelAttribute("task")Task task,
+                                   BindingResult result,
+                                    Model model) {
+    	List<Task> taskList = adminSrv.getAllTasks();
+        boolean nameExists = false;
+
+        for (Task t : taskList) {
+            if (t.getName().equalsIgnoreCase(task.getName())) {
+                nameExists = true;
+                break;
+            }
+        }
+
+        if (nameExists || result.hasErrors()) {
+            String messageError = "This task name already exists.";
+            model.addAttribute("messageError", messageError);
+        } 
+        else {
+            adminSrv.create(task);
+            String messageSuccess = "Task has been successfully created!";
+            model.addAttribute("messageSuccess", messageSuccess);
+        }
+        model.addAttribute("task", task);
+        return "task-create";
+             
     }
     @GetMapping("/delete/{id}")
     public String deleteTaskById(@PathVariable("id") Long id) {
@@ -107,11 +132,32 @@ public class AdminController {
  
     }
     @PostMapping(value = "/update/{id}")
-    public String updateTaskSubmit(@PathVariable("id") long id,
-                                   @ModelAttribute("task") Task task) {
-        adminSrv.update(task);
-        return "redirect:/taskList";
-       
+    public String updateTaskSubmit(@Valid @PathVariable("id") long id,
+                                   @ModelAttribute("task") Task task,
+                                   BindingResult result,
+                                   Model model) {
+    	List<Task> taskList = adminSrv.getAllTasks();
+        boolean nameExists = false;
+
+        for (Task t : taskList) {
+            if (t.getName().equalsIgnoreCase(task.getName())) {
+                nameExists = true;
+                break;
+            }
+        }
+
+        if (nameExists || result.hasErrors()) {
+            String messageError = "This task name already exists.";
+            model.addAttribute("messageError", messageError);
+        } 
+        else {
+            adminSrv.update(task);
+            String messageSuccess = "Task has been successfully updated!";
+            model.addAttribute("messageSuccess", messageSuccess);
+        }
+        model.addAttribute("task", task);
+        return "task-update";
+             
     }
 }
 
