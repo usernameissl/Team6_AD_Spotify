6257f73c551b06bcc05d8591231f8228a0771fc0
tanya <114603585+byewall@users.noreply.github.com>
2023-08-18 23:01:57 +0800

    Update FeedbackService.java

--- src/main/java/iss/ad/project/spotify/service/FeedbackService.java
+++ src/main/java/iss/ad/project/spotify/service/FeedbackService.java
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class FeedbackService {
-
+	@Autowired
+	private FeedbackRepo feedbackRepository;
 	public Feedback saveFeedback(Feedback feedback) {
 		return feedbackRepository.save(feedback);
 	}
