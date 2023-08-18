e2d4de858de33c900770c893d277b961d7e83838
byewall <bwvhazel@gmail.com>
2023-08-18 20:39:55 +0800

    added feedback

--- src/main/java/iss/ad/project/spotify/model/Feedback.java
+++ src/main/java/iss/ad/project/spotify/model/Feedback.java
@@ -1,34 +1,25 @@
 package iss.ad.project.spotify.model;
 
-import java.io.Serializable;
-
-import javax.persistence.*;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
 @Entity
-@Table(name="feedback")
-public class Feedback implements Serializable {
-	
+public class Feedback {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@Column(columnDefinition = "ENUM('UNHAPPY', 'NEUTRAL', 'SATISFIED')")
-    @Enumerated(EnumType.STRING)
-    private ExperienceEnum experience;
-	
-	private String comments;
-	
-	private String userName;
-	private int age;
-	
-	
 
+	private int rating;
+
+	private String comments;
 }
